9e243c256f514b3fd26d04fdb3025b7a5147474d
peyman <p.zonnouz@gmail.com>
2024-07-27 15:37:48 +0330

    fix connection reset error on front fetch

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 
@@ -25,9 +24,10 @@ func newServer(router *mux.Router) *server {
 
 func (s *server) serve() {
 	srv := http.Server{
-		Addr:              ":" + utils.GetEnv("port", "8000"),
-		Handler:           s.router,
-		ReadHeaderTimeout: time.Second}
+		Addr:    ":" + utils.GetEnv("port", "8000"),
+		Handler: s.router,
+		// ReadHeaderTimeout: time.Second
+	}
 	log.Printf("Starting Server on port%s", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
 }
